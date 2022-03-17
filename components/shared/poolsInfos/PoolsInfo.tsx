8426b96ad1b25739a7ee8d176220459354a04cf2
Charlie Rabiller <charlie.rabiller@loreal.com>
2022-03-17 14:33:16 -0400

    improvement suggested by conflux team

--- components/shared/poolsInfos/PoolsInfo.tsx
+++ components/shared/poolsInfos/PoolsInfo.tsx
@@ -15,7 +15,7 @@ export function PoolsInfo() {
           fill="currentColor"
           height="24"
           width="24"
-          className="w-16"
+          className="w-16 md:w-8"
           viewBox="0 0 24 24"
           color="neutral4"
         >
