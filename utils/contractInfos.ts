4d64b93c5e64480aefc6b91d56be7367ed39a660
Charlie85270 <rabiller.charlie@gmail.com>
2023-03-07 09:03:40 -0500

    add log fetch data

--- utils/contractInfos.ts
+++ utils/contractInfos.ts
@@ -162,6 +162,13 @@ export const getNucleonPoolInfos = async (
   const staker = 276;
   const apy = "18.66";
 
+  const stakerData = await fetch(
+    "https://mainapi.nucleon.network/api/v1/totalstakers"
+  );
+  const apyData = await fetch("https://mainapi.nucleon.network/api/v1/apy");
+
+  console.log(stakerData);
+  console.log(apyData);
   return {
     owner,
     contractInfo,
